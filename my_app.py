7c8265a25130dcadd23a855b710a64d13916ba95
diskAteka <102467071+diskAteka@users.noreply.github.com>
2022-03-27 16:48:44 +0500

    v0.0

---
+++ my_app.py
@@ -0,0 +1,3 @@
+# напиши здесь код основного приложения и первого экрана
+from PyQt.5.QWidgets import  QApplication, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout
+from PyQt.5.QtCore import Qt 
